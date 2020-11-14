52212170278e04b7a1271d4bfe97dc50d058e8c2
GuidoBallabio <guidodino.ballabio@gmail.com>
2020-11-14 23:58:30 +0100

    build(django): ignore migrations until stable with relative script

---
+++ delete_migrations.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+find . -path "**/migrations/*.py" -not -path "**/contrib/sites/*" -not -name "__init__.py" -delete
+find . -path "**/migrations/__pycache__/*" -delete
+find . -path "**/migrations/*.pyc" -delete
+
+find . -path "**/__pycache__/*" -delete
+
+python manage.py makemigrations
+
+mv esnpolimi_mgmt/migrations/0002_*.py esnpolimi_mgmt/migrations/0002_initial2.py
+mv 0003_createsuperuser.py esnpolimi_mgmt/migrations/
