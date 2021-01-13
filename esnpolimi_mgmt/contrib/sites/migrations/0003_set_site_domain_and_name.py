410cac1843e9b834fa578fbe519c10dc1129f064
GuidoBallabio <guidodino.ballabio@gmail.com>
2021-01-13 15:09:39 +0100

    fix(migration): set site domain to local in local env

--- esnpolimi_mgmt/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ esnpolimi_mgmt/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "mgmt.esnpolimi.it",
+            "domain": settings.ALLOWED_HOSTS[-1],
             "name": "ESNPolimi-mgmt"
         },
     )
