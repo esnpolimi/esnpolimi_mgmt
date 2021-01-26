1dcb935a32612ba46ff16fa72141cd8a5fb5ee51
GuidoBallabio <guidodino.ballabio@gmail.com>
2021-01-26 23:23:54 +0100

    fix(admin): reidrect to login page and then back if logged out

--- esnpolimi_mgmt/admin/__init__.py
+++ esnpolimi_mgmt/admin/__init__.py
@@ -1,5 +1,6 @@
 from django.contrib.admin import site
 from django.contrib.admin.views.decorators import staff_member_required
+from django.urls import reverse
 
 from .accounting import AccountAdmin, CashAdmin, TransactionAdmin  # noqa F401
 from .event import EventAdmin, MainListAdmin, WaitingListAdmin  # noqa F401
@@ -7,5 +8,5 @@ from .permission import PermissionAdmin  # noqa F401
 from .person import ESNcardAdmin, MatricolaAdmin, PersonAdmin  # noqa F401
 
 site.login = staff_member_required(
-    view_func=site.login, login_url="/", redirect_field_name=""
+    view_func=site.login, login_url=reverse("account_login")
 )
